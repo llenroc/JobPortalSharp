1caad46b05cfa7fb51244d92f086e4d34d0c84cc
rajeemcariazo <rajeem_cariazo@yahoo.com>
2016-08-26 07:29:21 +0800

    partial commit on job application new fields

--- JobPortalSharp.Data/Entities/JobApplication.cs
+++ JobPortalSharp.Data/Entities/JobApplication.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,8 +24,19 @@ namespace JobPortalSharp.Data
         public int Id { get; set; }
         public int JobPostId { get; set; }
         public JobPost JobPost { get; set; }
+
+        [Display(Name = "Application Date")]
         public DateTime? ApplicationDate { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
+
         public string CvFileName { get; set; }
         public string CvSystemFileName { get; set; }
         public string CoverLetterFileName { get; set; }
