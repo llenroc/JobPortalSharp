4404ae542552f393cf71fb63817616b8c99655a9
rajeemcariazo <rajeem_cariazo@yahoo.com>
2016-12-03 13:27:27 +0800

    log web api errors

--- JobPortalSharp/App_Start/WebApiConfig.cs
+++ JobPortalSharp/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using Elmah.Contrib.WebApi;
 
 namespace JobPortalSharp
 {
@@ -10,6 +12,7 @@ namespace JobPortalSharp
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Services.Add(typeof(IExceptionLogger), new ElmahExceptionLogger());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
